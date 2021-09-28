c02c6c7aa3559d66199056d0688e8f3a354ad308
sunrise30 <sunrise.30.yavis@gmail.com>
2021-09-28 07:37:25 -0500

    update deploy

---
+++ deploy/02_deploy_randomSVG.js
@@ -0,0 +1,20 @@
+module.exports = async ({
+  getNamedAccounts,
+  deployments,
+  getChainId
+}) => {
+  const { deploy, get, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+  let linkTokenAddress, vrfCoordinatorAddress;
+
+  if (chainId == 31337) {
+    let linkToken = await get('LinkToken');
+    linkTokenAddress = linkToken.address;
+    let vrfCoordinatorMock = await get('VRFCoordinatorMock');
+    vrfCoordinatorAddress = vrfCoordinatorMock.address;
+  } else {
+    
+  }
+}
